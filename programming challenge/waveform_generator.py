18bfbdfd71fba8ce73e0cc423d50fb4be1d24525
edruga <jhe6rv@virginia.edu>
2025-04-28 02:31:03 -0400

    even more tests!!!!! help

--- programming challenge/waveform_generator.py
+++ programming challenge/waveform_generator.py
@@ -35,6 +35,7 @@ note_frequencies = [
 
 min_third = pow(2.0, 3.0/12.0)
 maj_third = pow(2.0, 4.0/12.0)
+perf_fourth = pow(2.0, 5.0/12.0)
 
 
 # Note to self: need to choose duration and number of samples VERY carefully
@@ -48,22 +49,40 @@ waveform_list = []
 
 # waveform_list.append([1108.731, 1661.219, 2793.826])
 
-num_waveforms = 1000
+num_waveforms = 50
 for i in range(num_waveforms):
     root = note_frequencies[random.randint(0, 11)] # get random root
-    waveform_list.append([root, root * maj_third, root * min_third * maj_third]) # major chord from root
+    chord_type = random.randint(0, 2)
+    if chord_type == 0:
+        first_third = maj_third
+        second_third = min_third
+    elif chord_type == 1:
+        first_third = min_third
+        second_third = maj_third
+    else:
+        first_third = min_third
+        second_third = min_third
+    """
+    lo_hi = random.randint(0, 1)
+    if lo_hi == 0:
+        root = root * 4
+    else:
+        root = root / 4
+    """
+    waveform_list.append([root, root * perf_fourth, root * perf_fourth * first_third])
+    waveform_list.append([root, root * second_third, root * perf_fourth * second_third])
 
 
 # Number of evenly spaced samples to take of the composite waveform over the chosen time interval
 # For example, 100 samples over a 10-second interval would result in 10 samples per second
 # This number should be quite high to get appropriate waveform resolution
 # It should also be higher the greater the frequency or complexity of the waveform
-my_num_samples = 1024
+my_num_samples = 4096
 
 # Duration to sample the waveform
 # For high frequency waveforms, this can be quite short
 # for very low frequency waveforms, may want to sample a longer duration to capture periodicity
-my_duration = 0.1
+my_duration = 0.5
 
 # Get resultant samples of waveform from above parameters
 
@@ -71,7 +90,7 @@ result_list = []
 for freqs in waveform_list:
     result_list.append(waveform_generator(freqs, my_num_samples, my_duration))
 
-with open('testcases/test.in.20', 'w') as f:
+with open('testcases/test.in.14', 'w') as f:
     f.write(f"{len(result_list)} {my_num_samples} {my_duration:.1f}\n")
     for result in result_list:
         for sample in result:
