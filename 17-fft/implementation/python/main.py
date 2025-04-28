6f0e0bd810b5eb6ae24d2b0a8b3b815a7b011455
andbalch <andbalch@gmail.com>
2025-04-27 21:31:31 -0400

    submission file structure

---
+++ 17-fft/implementation/python/main.py
@@ -0,0 +1,52 @@
+from math import pi, sin, cos
+
+def dft(X):
+    N = len(X)
+    roots = [complex(real=cos(-2*pi*k/N), imag=sin(-2*pi*k/N)) for k in range(N)]
+    Y = []
+    for k in range(N):
+        Y.append(sum([X[n] * roots[n*k % N] for n in range(N)]))
+    return Y
+
+def fft(X: list[float]) -> list[float]:
+    N = len(X)
+    if N == 1:
+        return X 
+    Xodd = []
+    Xeven = []
+    for i in range(N):
+        if i % 2 == 0:
+            Xeven.append(X[i])
+        else:
+            Xodd.append(X[i])
+    Yodd = fft(Xodd)
+    Yeven = fft(Xeven)
+    Y = [0] * N
+    arg = -2*pi/N
+    for k in range(N//2):
+        root = complex(real=cos(arg*k), imag=sin(arg*k))
+        Y[k] = Yeven[k] + root*Yodd[k]
+        Y[k+N//2] = Yeven[k] - root*Yodd[k]
+    return Y
+
+def main():
+    # X = [2*cos(x) + cos(2*x) for x in range(1024)]
+    # Yfft = fft(X)
+    # Ydft = dft(X)
+    # for i in range(len(X)):
+    #     print(f"{Yfft[i].real}\t{Ydft[i].real}")
+    # print(all([abs(Yfft[i] - Ydft[i]) <= 1e-5 for i in range(len(Yfft))]))
+
+    n = int(input())
+    X = list()
+    for i in range(n):
+        X.append(float(input()))
+    
+    Y = fft(X)
+    for i in range(len(Y)):
+        print(f"({Y[i].real:0,.5f}, {Y[i].imag:0,.5f})")
+        
+
+if __name__=='__main__':
+    main()
+
