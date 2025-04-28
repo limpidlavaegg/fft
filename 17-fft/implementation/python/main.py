9b310398c727a39a0acccf99231900127713b3b2
andbalch <andbalch@gmail.com>
2025-04-27 23:05:27 -0400

    comments for base java implementation

--- 17-fft/implementation/python/main.py
+++ 17-fft/implementation/python/main.py
@@ -1,17 +1,13 @@
 from math import pi, sin, cos
 
-def dft(X):
-    N = len(X)
-    roots = [complex(real=cos(-2*pi*k/N), imag=sin(-2*pi*k/N)) for k in range(N)]
-    Y = []
-    for k in range(N):
-        Y.append(sum([X[n] * roots[n*k % N] for n in range(N)]))
-    return Y
-
 def fft(X: list[float]) -> list[float]:
     N = len(X)
+
+    # Base case: in contains just one element -> the result of the transform is the element itself
     if N == 1:
         return X 
+    
+    # Split array into odd and even indices to calculate a DFT via divide-and-conquer
     Xodd = []
     Xeven = []
     for i in range(N):
@@ -19,24 +15,26 @@ def fft(X: list[float]) -> list[float]:
             Xeven.append(X[i])
         else:
             Xodd.append(X[i])
+
+    # Recursively calculate the DFT on odds and evens separately, we will merge the results below
     Yodd = fft(Xodd)
     Yeven = fft(Xeven)
+
+    # Init an array of all 0s of size N to store the results of the merge operation
     Y = [0] * N
+    # Define constant value for use in calculating the twiddle factors
     arg = -2*pi/N
     for k in range(N//2):
+        # Calculate the twiddle factor: exp(-2im*pi*k/n)
+        # This approach to calculating the twiddle factor uses sine and cosine waves to define the real and imaginary components of the factor, respectively, rather than taking the complex exponential. However, the two approaches are equivalent
         root = complex(real=cos(arg*k), imag=sin(arg*k))
+        # Front half of the result: even_k + exp(-2im*pi*k/n) * odd_k
         Y[k] = Yeven[k] + root*Yodd[k]
+        # Back half of the result: even_k - exp(-2im*pi*k/n) * odd_k
         Y[k+N//2] = Yeven[k] - root*Yodd[k]
     return Y
 
 def main():
-    # X = [2*cos(x) + cos(2*x) for x in range(1024)]
-    # Yfft = fft(X)
-    # Ydft = dft(X)
-    # for i in range(len(X)):
-    #     print(f"{Yfft[i].real}\t{Ydft[i].real}")
-    # print(all([abs(Yfft[i] - Ydft[i]) <= 1e-5 for i in range(len(Yfft))]))
-
     n = int(input())
     X = list()
     for i in range(n):
